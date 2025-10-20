315b1fac19f938f53c58cb365db95d5fb6f8c261
hammad-Ibrahim <hammad.ibrahim21994@gmail.com>
2025-10-20 16:22:43 +0300

    Product Variant ViewSet (Issue #3: Public Read-Only)

--- catalog/views.py
+++ catalog/views.py
@@ -43,5 +43,37 @@ class ATCClassViewSet(viewsets.ModelViewSet):
 # --------------
 # Product ViewSet (Issue #4: Admin CRUD for Parent Product)
 # -------------
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    CRUD API for product (Brand) entity.
+    Handles nested creation /update if ingredients via overridden serializer methods.
+    """
+    queryset = Product.objects.prefetch_related('active_ingredients').all()
+    serializer_class = ProductSerializer
+    # Enforce Gherkin : only authentication admins can perform CRUD
+    permission_classes = [permissions.IsAdminUser]
 
 
+# ---------------
+# Product Variant ViewSet (Issue #3: Public Read-Only)
+# --------------
+
+class ProductVariantViewSet(viewsets.ReadOnlyModelViewSet):
+    """Public read-only API for browsing and searching sellable units."""
+    queryset = ProductVariant.objects.select_related('product', 'dosage_form').all()
+    serializer_class = ProductVariantSerializer
+
+    #Public access : anyone can read the catalog
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    #Enable filtering and searching
+    filter_backends=[DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    #search fields (searching by brand name , ingredient, etc).
+    search_fields = ['product__brand_name', 'product__active_ingredients__name','barcode_gtin' ]
+
+    #Filter fields (for advanced sidebar filtering
+    filterset_fields =['is_prescription_only', 'is_otc', 'dosage_form']
+
+    #Ordering fields (e.g., sorting by name or pack size)
+    ordering_fields= ['product__brand_name', 'pack_size']
